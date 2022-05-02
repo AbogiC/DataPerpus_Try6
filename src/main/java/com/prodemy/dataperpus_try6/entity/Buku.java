d921b4498314975a23e8aa1922e7810ee5ceea98
Abednego Bogi Christian <99196129+AbogiC@users.noreply.github.com>
2022-05-02 19:34:58 +0700

    add more files

--- src/main/java/com/prodemy/dataperpus_try6/entity/Buku.java
+++ src/main/java/com/prodemy/dataperpus_try6/entity/Buku.java
@@ -1,2 +1,100 @@
-package com.prodemy.dataperpus_try6.entity;public class Buku {
+package com.prodemy.dataperpus_try6.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_buku")
+public class Buku {
+    @Id
+    @Column(name = "id_buku", length = 25)
+    private String idBuku;
+    @Column(name = "judul_buku")
+    private String judulBuku;
+    @Column(name = "penulis")
+    private String penulisBuku;
+    @Column(name = "penerbit")
+    private String penerbitBuku;
+    @Column(name = "tahun_terbit")
+    private Integer tahunTerbit;
+    @Column(name = "kategori_buku")
+    private String namaKategori;
+    @Column(name = "jumlah_buku")
+    private Integer jumlahBuku;
+    @Column(name = "stok")
+    private Integer stokBuku;
+    @ManyToOne
+    @JoinColumn(name = "rak", nullable = false)
+    private Rak rak;
+
+    public String getIdBuku() {
+        return idBuku;
+    }
+
+    public void setIdBuku(String idBuku) {
+        this.idBuku = idBuku;
+    }
+
+    public String getJudulBuku() {
+        return judulBuku;
+    }
+
+    public void setJudulBuku(String judulBuku) {
+        this.judulBuku = judulBuku;
+    }
+
+    public String getPenulisBuku() {
+        return penulisBuku;
+    }
+
+    public void setPenulisBuku(String penulisBuku) {
+        this.penulisBuku = penulisBuku;
+    }
+
+    public String getPenerbitBuku() {
+        return penerbitBuku;
+    }
+
+    public void setPenerbitBuku(String penerbitBuku) {
+        this.penerbitBuku = penerbitBuku;
+    }
+
+    public Integer getTahunTerbit() {
+        return tahunTerbit;
+    }
+
+    public void setTahunTerbit(Integer tahunTerbit) {
+        this.tahunTerbit = tahunTerbit;
+    }
+
+    public String getNamaKategori() {
+        return namaKategori;
+    }
+
+    public void setNamaKategori(String namaKategori) {
+        this.namaKategori = namaKategori;
+    }
+
+    public Integer getJumlahBuku() {
+        return jumlahBuku;
+    }
+
+    public void setJumlahBuku(Integer jumlahBuku) {
+        this.jumlahBuku = jumlahBuku;
+    }
+
+    public Integer getStokBuku() {
+        return stokBuku;
+    }
+
+    public void setStokBuku(Integer stokBuku) {
+        this.stokBuku = stokBuku;
+    }
+
+    public Rak getRak() {
+        return rak;
+    }
+
+    public void setRak(Rak rak) {
+        this.rak = rak;
+    }
 }
--- src/main/java/com/prodemy/dataperpus_try6/entity/Denda.java
+++ src/main/java/com/prodemy/dataperpus_try6/entity/Denda.java
@@ -1,2 +1,40 @@
-package com.prodemy.dataperpus_try6.entity;public class Denda {
+package com.prodemy.dataperpus_try6.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_denda")
+public class Denda {
+    @Id
+    @Column(name = "kode_denda")
+    private String kodeDenda;
+    @Column
+    private int besarDenda;
+    @OneToOne
+    @JoinColumn
+    private Transaksi kodeTransaksi;
+
+    public String getKodeDenda() {
+        return kodeDenda;
+    }
+
+    public void setKodeDenda(String kodeDenda) {
+        this.kodeDenda = kodeDenda;
+    }
+
+    public int getBesarDenda() {
+        return besarDenda;
+    }
+
+    public void setBesarDenda(int besarDenda) {
+        this.besarDenda = besarDenda;
+    }
+
+    public Transaksi getKodeTransaksi() {
+        return kodeTransaksi;
+    }
+
+    public void setKodeTransaksi(Transaksi kodeTransaksi) {
+        this.kodeTransaksi = kodeTransaksi;
+    }
 }
